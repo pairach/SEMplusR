262e3ea99ed2f9d465eb5ce6b1299244328d7a70
Pairach Piboonrungroj <me@pairach.com>
2012-06-06 03:12:13 +0100

    Update sort_mi function
    Add k in the function to specify the cut-off value of MI

--- sort_mi.R
+++ sort_mi.R
@@ -1,9 +1,15 @@
 # A function to sort path or cov that provide highest modification index (MI) decendingly.
-# One may specify number of paths and/or cov in the putput
+# One may specify number of paths and/or cov in the output as n
+# One may specify a cut-off value of MI as k
 
-sort_mi <- function(fit, n) # fit = fitted model, n = number of suggested paths or cov
+sort_mi <- function(fit, n=n, k=k) 
+  # fit = fitted model, 
+  # n = number of suggested paths or cov,
+  # k = cut-off value of MI
 {
-  s.mi <- inspect(fit, what = "mi")
-  sort.mi <- s.mi[order(-s.mi$mi),]
+  sort.mi <- inspect(fit, what = "mi")
+  sort.mi <- sort.mi[order(-sort.mi$mi),]
+  sort.mi <- data.frame(sort.mi)
+  sort.mi <- sort.mi[which(sort.mi$mi > k),]
   sort.mi[1:n,]
 }
