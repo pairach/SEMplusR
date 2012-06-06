0adb20e66c9b89994f3c7dcc1918dbc2397ad7dd
Pairach Piboonrungroj <me@pairach.com>
2012-06-06 02:36:01 +0100

    Update sort_mi function

--- sort_mi.R
+++ sort_mi.R
@@ -1,8 +1,9 @@
-sortMi <- function(fit, n) # fit = fitted model, n = number of suggested paths or cov
+# A function to sort path or cov that provide highest modification index (MI) decendingly.
+# One may specify number of paths and/or cov in the putput
+
+sort_mi <- function(fit, n) # fit = fitted model, n = number of suggested paths or cov
 {
   s.mi <- inspect(fit, what = "mi")
   sort.mi <- s.mi[order(-s.mi$mi),]
   sort.mi[1:n,]
 }
-#test sortMi function
-sort_mi(fit.TR, 5)
